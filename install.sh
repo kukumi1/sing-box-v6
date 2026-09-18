e551a02d3cdf55b5b2fde1f27cf06b93bb5b460a
kukumi <79169995+kukumi1@users.noreply.github.com>
2026-09-18 17:19:10 +0800

    Refresh interactive installer entrypoint

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Short public entrypoint for the sing-box IPv6/NAT installer.
 set -eu
-url='https://raw.githubusercontent.com/kukumi1/sing-box-v6/main/scripts/vps-node.sh'
+url='https://raw.githubusercontent.com/kukumi1/sing-box-v6/main/scripts/vps-node.sh?v=interactive'
 curl -fsSL "$url" | sh -s -- "$@"
+
